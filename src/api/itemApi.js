c936aa30aefe1a676934d2051dacc7160e82a720
yethminivisandi <yethminivisandi11@gmail.com>
2026-04-26 12:09:54 +0530

    up

--- src/api/itemApi.js
+++ src/api/itemApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://wmtbackend-production-ea55.up.railway.app/",
+  baseURL: import.meta.env.VITE_API_URL 
 });
 
 export const getItems = () => API.get("/items");
